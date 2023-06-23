29df2b3b60284ce696b855f381dd69a525f0d140
StrongVector <vector.strong@protonmail.com>
2023-06-22 22:43:29 -0700

    fifth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
